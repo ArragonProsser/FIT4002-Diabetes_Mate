e00b5bbe4c0fdab25eaba15a5b9a075f9eaa30cd
Akila Helitha Paranawithana <akila.codes@gmail.com>
2023-08-28 17:31:46 +1000

    Fixed bugs in appointmentsLambda

--- amplify/backend/function/appointmentsLambda/src/index.js
+++ amplify/backend/function/appointmentsLambda/src/index.js
@@ -1,4 +1,17 @@
-const fun = require('/opt/appointments.controller.js');
+/* Amplify Params - DO NOT EDIT
+	AUTH_DIABETESMATE_USERPOOLID
+	ENV
+	REGION
+	STORAGE_APPOINTMENT_ARN
+	STORAGE_APPOINTMENT_NAME
+	STORAGE_APPOINTMENT_STREAMARN
+	STORAGE_ARTICLE_ARN
+	STORAGE_ARTICLE_NAME
+	STORAGE_ARTICLE_STREAMARN
+	STORAGE_USER_ARN
+	STORAGE_USER_NAME
+	STORAGE_USER_STREAMARN
+Amplify Params - DO NOT EDIT */const fun = require('/opt/appointments.controller.js');
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
